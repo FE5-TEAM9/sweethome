e4196c60acd7dd29855e29ea26349fbc18c5b891
hbsongk <68498323+hbsongk@users.noreply.github.com>
2023-06-14 10:55:58 +0900

    Feat(fe): 캐러셀 골격 구현

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,9 +1,10 @@
 import styles from "~/styles/Home.module.scss";
-
+import { Slider } from "~/utils/slider.tsx";
 const Home = () => {
   return (
     <>
       <div className={styles.main}>
+        <Slider />
         <div className={styles.notice}>
           <span className={styles.title}>Notice</span>
           <span>사진 리뷰 작성 시 5000p 지급 이벤트를 확인해주세요.</span>
