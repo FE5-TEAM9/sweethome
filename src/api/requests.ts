1bdc6c65fec1275065e74c290d10b40bacf4dc0a
ChoEun-Sang <wh94es@gmail.com>
2023-06-14 19:38:29 +0900

    Feat(fe): 상품 등록 API 추가

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -11,13 +11,13 @@
 // })
 
 // Sign-Up 회원가입
-interface RequestBody {
+interface SignUpBody {
   email: string
   password: string
   displayName: string
 }
 
-const signUp = async (body: RequestBody) => {
+const signUp = async (body: SignUpBody) => {
   const res = await fetch(
     'https://asia-northeast3-heropy-api.cloudfunctions.net/api/auth/signup',
     {
@@ -35,4 +35,34 @@ const signUp = async (body: RequestBody) => {
   return data
 }
 
-export { signUp }
+// Add-Products 상품등록
+interface AddProductsBody {
+  title: string
+  price: number
+  description: string
+  tags?: string
+  thumbnailBase64?: string 
+  photoBase64?: string 
+  discountRate?: number 
+}
+
+const addProducts = async (body: AddProductsBody) => {
+  const res = await fetch(
+    'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products',
+    {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json',
+        'apikey': 'KDT5_nREmPe9B',
+        'username': 'KDT5_Team9',
+        'masterKey': true
+      },
+      body: JSON.stringify(body)
+    }
+   )
+  const data = await res.json()
+  console.log(data)
+  return data
+}
+
+export { signUp, addProducts }
