b031f35805fb2f91ea174ff7b537a32c72b2403f
hbsongk <68498323+hbsongk@users.noreply.github.com>
2023-06-14 17:30:58 +0900

    Design(fe): 로그인 페이지 스타일 변경

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -14,43 +14,43 @@ const Login = () => {
   return (
     <>
       <section className={styles.login}>
-        <h2 className={styles.login__title}>로그인</h2>
-        <div className={styles.login__form}>
-          <form onSubmit={e => signIn(e, body)}>
-            <div className={styles.input__id}>
-              <span>ID</span>
-              <input
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                placeholder="이메일을 입력하세요."
-              />
+        <div className={styles.wrapper}>
+          <div className={styles.form}>
+            <h2>로그인</h2>
+            <form onSubmit={e => signIn(e, body)}>
+              <div className={styles.id}>
+                <span>ID</span>
+                <input
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  placeholder="이메일을 입력하세요."
+                />
+              </div>
+              <div className={styles.pwd}>
+                <span>PW</span>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </div>
+              <button
+                type="submit"
+                className={styles.btn__login}>
+                로그인
+              </button>
+            </form>
+            <div className={styles.find}>
+              <p>아이디 찾기</p>
+              <p>|</p>
+              <p>비밀번호 찾기</p>
             </div>
-            <div className={styles.input__pwd}>
-              <span>PW</span>
-              <input
-                type="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-              />
-            </div>
-            <button
-              type="submit"
-              className={styles.btn__login}>
-              로그인
-            </button>
-          </form>
-          <div className={styles.login__findaccount}>
-            <p>아이디 찾기</p>
-            <p>|</p>
-            <p>비밀번호 찾기</p>
           </div>
-        </div>
-        <div className={styles.signup}>
-          <p className={styles.signup__notice}>아직 회원이 아니신가요?</p>
-          <p className={styles.signup__notice}>
-            회원 가입을 하시면 다양하고 특별한 혜택을 누리세요
-          </p>
-          <button className={styles.btn__signup}>회원가입</button>
+          <div className={styles.signup}>
+            <p>아직 회원이 아니신가요?</p>
+            <p>회원 가입을 하시면 다양하고 특별한 혜택을 누리세요</p>
+            <button className={styles.btn__signup}>회원가입</button>
+          </div>
         </div>
       </section>
     </>
