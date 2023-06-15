b7142c44ef5b70c8cfd0cd68fc1a15662b351488
hbsongk <68498323+hbsongk@users.noreply.github.com>
2023-06-15 14:35:53 +0900

    Chore(fe): API 함수 정리 및 수정

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,65 +1,27 @@
-// const options = (method, param) => ({
-//   method,
-//   headers: {
-//     'content-type': 'application/json',
-//     'apikey': 'KDT5_nREmPe9B',
-//     'username': 'KDT5_Team9'
-//   },
-//   body: JSON.stringify(param)
-// })
-
-// Sign-Up 회원가입
-interface User {
-  email: string;
-  password: string;
-}
-interface RequestBody extends User {
-  displayName: string;
-}
-interface SignUpBody {
-  email: string
-  password: string
-  displayName: string
-}
-// Add-Products 상품등록
-interface AddProductsBody {
-  title: string
-  price: number
-  description: string
-  tags?: string
-  thumbnailBase64?: string 
-  photoBase64?: string 
-  discountRate?: number 
-}
+// Common Header
 const headers = {
   "content-type": "application/json",
   apikey: "KDT5_nREmPe9B",
   username: "KDT5_Team9"
 };
-const addProducts = async (body: AddProductsBody) => {
-  const res = await fetch(
-    'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products',
-    {
-      method: "POST",
-      headers: {
-        'content-type': 'application/json',
-        'apikey': 'KDT5_nREmPe9B',
-        'username': 'KDT5_Team9',
-        'masterKey': 'true'
-      },
-      body: JSON.stringify(body)
-    }
-   )
-  const data = await res.json()
-  console.log(data)
-  return data
+
+// Common Interface
+interface User {
+  email: string;
+  password: string;
 }
-const signUp = async (body: RequestBody) => {
+
+// Sign-Up 회원가입
+interface SignUpBody extends User {
+  displayName: string;
+}
+
+const signUp = async (body: SignUpBody) => {
   const res = await fetch(
     "https://asia-northeast3-heropy-api.cloudfunctions.net/api/auth/signup",
     {
       method: "POST",
-      headers: { ...headers },
+      headers,
       body: JSON.stringify(body)
     }
   );
@@ -67,7 +29,9 @@ const signUp = async (body: RequestBody) => {
   console.log(data);
   return data;
 };
-const signIn = async (event: React.FormEvent, body: User) => {
+
+// Log-In 로그인
+const logIn = async (event: React.FormEvent, body: User) => {
   event.preventDefault();
   const res = await fetch(
     "https://asia-northeast3-heropy-api.cloudfunctions.net/api/auth/login",
@@ -81,7 +45,9 @@ const signIn = async (event: React.FormEvent, body: User) => {
   console.log(json);
   localStorage.setItem("token", json.accessToken);
 };
-const signOut = async () => {
+
+// Log-Out 로그아웃
+const logOut = async () => {
   const res = await fetch(
     "https://asia-northeast3-heropy-api.cloudfunctions.net/api/auth/logout",
     {
@@ -95,4 +61,35 @@ const signOut = async () => {
   const json = await res.json();
   console.log(json);
 };
-export { signUp, signIn, signOut, addProducts };
+
+// Add-Products 상품등록
+interface AddProductsBody {
+  title: string;
+  price: number;
+  description: string;
+  tags?: string;
+  thumbnailBase64?: string;
+  photoBase64?: string;
+  discountRate?: number;
+}
+
+const addProducts = async (body: AddProductsBody) => {
+  const res = await fetch(
+    "https://asia-northeast3-heropy-api.cloudfunctions.net/api/products",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        apikey: "KDT5_nREmPe9B",
+        username: "KDT5_Team9",
+        masterKey: "true"
+      },
+      body: JSON.stringify(body)
+    }
+  );
+  const data = await res.json();
+  console.log(data);
+  return data;
+};
+
+export { signUp, logIn, logOut, addProducts };
