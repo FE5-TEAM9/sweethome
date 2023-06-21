e890ac18da96651153f89e52f656123c3a9a9cb2
junehee <dev.junehee@gmail.com>
2023-06-21 21:47:17 +0900

    Feat(fe): Shop 상품 리스트 filter 기능 추가

--- src/routes/Shop/Shop.tsx
+++ src/routes/Shop/Shop.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { getAllProducts } from '~/api/requests';
 import ProductItem from '~/components/Shop/ProductItem';
@@ -19,38 +19,45 @@ const Shop = () => {
   }
 
   const [allProducts, setAllProducts] = useState<ResponseValue>([]);
+  const [originalProducts, setOriginalProducts] = useState();
 
   useEffect(() => {
     spreadAllProducts();
   }, [])
 
+  useEffect(() => {
+  }, [allProducts])
+
   const spreadAllProducts = async () => {
     try {
       const res = await getAllProducts();
       setAllProducts(res);
+      setOriginalProducts(res);
       console.log('전체 상품 정보', res);
     } catch (error) {
       console.log('상품 불러오기 실패', error);
     }
   }
-  // const category = [ "All", "FURNITURE", "KITCHEN", "BEDROOM" ];
-  // const [click, setClick] = useState(false);
+
+  const categorys = [ "ALL", "FURNITURE", "KITCHEN", "BEDROOM" ];
+  const [click, setClick] = useState(false);
 
 
-  // const clickHandler = (e: React.MouseEvent<HTMLInputElement>) => {
-  //   setClick(!click)
-  // }
+  const clickHandler = (e: React.MouseEvent<HTMLInputElement>) => {
+    setClick(!click)
+  }
 
-  // const categoryHandler = (e) => {
-  //   if (All 눌렀어) {
-  //     ...모든
-      
-  //   } else if (Furni) {
-  //     상품들.filter(tag => tag.name === FUrniture)
-  //   }
-  // }
-
-  
+  const categoryHandler = (e: React.MouseEvent<HTMLInputElement>) => {
+    if (e.target.value === "ALL") {
+      setAllProducts(originalProducts)
+    } else if (e.target.value === "FURNITURE") {
+      setAllProducts(originalProducts.filter(product => product.tags === "furniture"))
+    } else if (e.target.value === "KITCHEN") {
+      setAllProducts(originalProducts.filter(product => product.tags === "kitchen"))
+    } else if (e.target.value === "BEDROOM") {
+      setAllProducts(originalProducts.filter(product => product.tags === "bedroom"))
+    }
+  }
 
   return (
     <>
@@ -58,17 +65,25 @@ const Shop = () => {
         <div className={styles.menuWrap}>
           <ul className={styles.menu}>
             <li>
+              {categorys.map(category => (
+                <input type='button' value={category} onClick={categoryHandler} />
+              ))}
+            </li>
+            {/* <li>
               <input type='button' value="All" />
             </li>
             <li>
-              <input type='button' value="FURNITURE" />
+              <input
+                type='button'
+                value="FURNITURE"
+                onClick={categoryHandler} />
             </li>
             <li>
               <input type='button' value="KITCHEN" />
             </li>
             <li>
               <input type='button' value="BEDROOM" />
-            </li>
+            </li> */}
           </ul>
         </div>
         <div className={styles.productListWrap}>
