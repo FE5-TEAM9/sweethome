a08a16ea61f79d6dc18ea6ec9c940d5921df1bee
ChoEun-Sang <wh94es@gmail.com>
2023-06-22 17:57:37 +0900

    Style(fe): 마이페이지 스타일 수정

--- src/components/MyPage/Account/Account.tsx
+++ src/components/MyPage/Account/Account.tsx
@@ -7,19 +7,24 @@ const Account = ({ item, watch, setWatch }) => {
   const [deletItem, setDeleteItem ] = useState('');
   
   // 계좌 해지
-const deleteAcountHandler = async (id) => {
-  const body ={
-    accountId: id,
-    signature: true
+  const deleteAcountHandler = async (id) => {
+    const body ={
+      accountId: id,
+      signature: true
+    }
+    try {
+      const res = await deleteAccount(body);
+      console.log('계좌 삭제',res)
+      setWatch(!watch)
+    } catch (error) {
+      alert('계좌 해지 실패하였습니다.')
+    }
   }
-  try {
-    const res = await deleteAccount(body);
-    console.log('계좌 삭제',res)
-    setWatch(!watch)
-  } catch (error) {
-    alert('계좌 해지 실패하였습니다.')
+
+  // 금액 단위 표시
+  const convertPrice = (price: number) => {
+    return price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
-}
 
   return (
     <div className={styles.account} >
@@ -27,7 +32,7 @@ const deleteAcountHandler = async (id) => {
         <div className={styles.inner}>
           <h4>{item.bankName}</h4>
           <p>{item.accountNumber}</p>
-          <span>{`잔액: ${item.balance}`}</span>
+          <span>{`잔액: ${convertPrice(item.balance)}원`}</span>
         </div>
         <TiDeleteOutline className={styles.deleteBtn} onClick={()=>deleteAcountHandler(item.id)} />
       </div>
