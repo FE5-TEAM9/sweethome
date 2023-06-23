559d2d64fd0e7b68f4c39ad51843cadb71c6e949
junehee <dev.junehee@gmail.com>
2023-06-24 00:48:24 +0900

    Design(fe): About 페이지 대표 이미지, 소개글 추가

--- src/utils/slider.tsx
+++ src/utils/slider.tsx
@@ -1,9 +1,9 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y, EffectCube } from "swiper";
 import "swiper/swiper-bundle.min.css";
-import main1 from "/public/assets/main/main1.jpeg";
-import main2 from "/public/assets/main/main2.jpeg";
-import main3 from "/public/assets/main/main3.jpeg";
+import main1 from "/public/assets/Home/main1.jpeg";
+import main2 from "/public/assets/Home/main2.jpeg";
+import main3 from "/public/assets/Home/main3.jpeg";
 import styles from "~/styles/utils/Slider.module.scss";
 
 export const Slider = ({}) => {
