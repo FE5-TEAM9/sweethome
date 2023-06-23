7f7750a26688e51bc506657102be17d9c7d7ed6f
hbsongk <68498323+hbsongk@users.noreply.github.com>
2023-06-23 12:24:17 +0900

    Feat(fe): combineReducers안에 logout 리듀서 추가

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,14 +2,17 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import info from "./info";
 import cart from "./cart";
+import logout from "./logout";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
   key: "root",
   storage,
-  whiteList: ["info", "cart"]
+  whiteList: ["info", "cart", "logout"]
 };
 const rootReducer = combineReducers({
-  info, cart
+  info,
+  cart,
+  logout
 });
 export default persistReducer(persistConfig, rootReducer);
