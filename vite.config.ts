4ff77be49ca00c675a03d84276cb1138d6db0c05
junehee <dev.junehee@gmail.com>
2023-07-01 13:29:50 +0900

    Chore(fe): vite config baseUrl 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sweethome/",
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src")
