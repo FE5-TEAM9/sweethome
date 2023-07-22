2872380ebbfc38ec1212b6c1b8359aac3023479d
junehee <dev.junehee@gmail.com>
2023-07-22 20:43:57 +0900

    Rename(fe): MyPage 폴더명 변경

---
+++ src/routes/MyPage/MyBankAccount.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getBankList, getAccountList } from "~/api/user";
+import Account from "~/components/MyPage/MyBankAccount/Account";
+import AccountModal from "~/components/MyPage/MyBankAccount/AccountModal";
+import Loading from "~/components/common/Loading";
+import styles from "~/styles/MyPage/MyBankAccount.module.scss";
+import { Bank } from "~/types";
+
+const MyBankAccount = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [bankList, setBankList] = useState([]);
+  const [accountList, setAccountList] = useState<Bank[]>([]);
+  const [watch, setWatch] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    checkBankList();
+    checkAccountList();
+  }, [watch]);
+
+  // 선택 가능한 은행 목록 조회
+  const checkBankList = async () => {
+    setIsLoading(true);
+    try {
+      const res = await getBankList();
+      setBankList(res);
+    } catch (error) {
+      alert("은행 정보를 불러오는 데 실패하였습니다.");
+    }
+    setIsLoading(false);
+  };
+
+  // 등록된 계좌 조회
+  const checkAccountList = async () => {
+    setIsLoading(true);
+    try {
+      const res = await getAccountList();
+      setAccountList(res.accounts);
+      dispatch({ type: "GET_ACCOUNT_LIST", accountList: res });
+    } catch (error) {
+      alert("계좌 정보 불러오는 데 실패하였습니다.");
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      {isLoading ? <Loading /> : null}
+      {showModal && (
+        <AccountModal
+          bankList={bankList}
+          // onFormCancel={onFormCancel}
+          watch={watch}
+          setWatch={setWatch}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
+      )}
+      <section className={styles.accountList}>
+        <div className={styles.accountListContainer}>
+          <div className={styles.title}>
+            <h2>계좌 관리</h2>
+          </div>
+          {accountList.length > 0 ? (
+            accountList.map((item, i: number) => (
+              <Account
+                key={i}
+                item={item}
+                watch={watch}
+                setWatch={setWatch}
+              />
+            ))
+          ) : (
+            <div className={styles.noAccount}>
+              <div className={styles.textWrap}>
+                <p>등록된 계좌가 없습니다.</p>
+                <p>계좌 번호를 등록해 주세요!</p>
+              </div>
+            </div>
+          )}
+          <button
+            className={styles.btn}
+            onClick={() => setShowModal(!showModal)}>
+            계좌 연결
+          </button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default MyBankAccount;
---
+++ src/routes/MyPage/MyOrder.tsx
@@ -0,0 +1,184 @@
+import { useEffect, useState } from "react";
+import {
+  cancelTransaction,
+  confirmedTransaction,
+  getAllTransactions,
+  getTransaction
+} from "~/api/products";
+import { convertPrice, convertDate, sortDate } from "~/utils/convert";
+import MyOrderDetails from "~/routes/MyPage/MyOrderDetails";
+import Loading from "~/components/common/Loading";
+import styles from "~/styles/MyPage/MyOrder.module.scss";
+import { AllTransactions, AllTransactionDetail } from "~/types";
+
+const MyOrder = () => {
+  const [allList, setAllList] = useState<AllTransactions>([]);
+  const [showDetails, setShowDetails] = useState(false);
+  const [orderDetails, setOrderDetails] = useState<any>({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [watch, setWatch] = useState(false);
+
+  useEffect(() => {
+    allTransactions();
+  }, [watch]);
+
+  // 전체 거래 내역
+  const allTransactions = async () => {
+    setIsLoading(true);
+    const allRes = await getAllTransactions();
+    if (allRes) {
+      setAllList(
+        allRes
+          .filter((res: AllTransactionDetail) => !res.isCanceled)
+          .sort(
+            (a: AllTransactionDetail, b: AllTransactionDetail) =>
+              sortDate(b.timePaid) - sortDate(a.timePaid)
+          )
+      );
+    } else {
+      alert("주문 내역이 없습니다.");
+    }
+    setIsLoading(false);
+  };
+
+  // 구매 취소
+  const cancelHandler = async (
+    e: React.MouseEvent<HTMLInputElement>,
+    id: string
+  ) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const body = { detailId: id };
+      const res = await cancelTransaction(body);
+      if (res) {
+        setWatch(!watch);
+        alert("구매 취소되었습니다.");
+      } else {
+        alert("구매 확정된 상품을 취소할 수 없습니다.");
+      }
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // 구매 확정
+  const confirmHandler = async (
+    e: React.MouseEvent<HTMLInputElement>,
+    Id: string
+  ) => {
+    e.preventDefault();
+    try {
+      const body = { detailId: Id };
+      const res = await confirmedTransaction(body);
+      if (res) {
+        setWatch(!watch);
+        alert("구매 확정 완료!");
+      }
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  // 단일 상품 상세
+  const showDetailsHandler = async (
+    e: React.MouseEvent<HTMLInputElement>,
+    id: string
+  ) => {
+    e.preventDefault();
+    try {
+      const body = { detailId: id };
+      const res = await getTransaction(body);
+      setOrderDetails(res);
+      setShowDetails(true);
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      {isLoading ? <Loading /> : null}
+      {showDetails ? (
+        <MyOrderDetails
+          setShowDetails={setShowDetails}
+          orderDetails={orderDetails}
+        />
+      ) : null}
+      <section className={styles.myOrder}>
+        <div className={styles.container}>
+          <div className={styles.title}>
+            <h2>주문 내역 관리</h2>
+          </div>
+
+          <div className={styles.list_nav}>
+            <div className={styles.list_nav_container}>
+              <div>NO.</div>
+              <div>상품 이미지</div>
+              <div>상품명</div>
+              <div>상품 가격</div>
+              <div>주문 일자</div>
+              <div>구매 확정</div>
+              <div>비고</div>
+            </div>
+          </div>
+
+          <div className={styles.content}>
+            <ul className={styles.allList}>
+              {allList.map((list: any, i) => (
+                <li
+                  className={styles.list}
+                  key={list.datailId}>
+                  <span>{i + 1}</span>
+                  <div className={styles.listImg}>
+                    <img
+                      src={list.product.thumbnail}
+                      alt={list.product.title}
+                    />
+                  </div>
+                  <span>{list.product.title}</span>
+                  <span>{convertPrice(list.product.price)}</span>
+                  <span>{convertDate(list.timePaid)}</span>
+                  <span>{list.done ? "✅" : "❌"}</span>
+                  <div className={styles.listBtn}>
+                    {list.done ? null : (
+                      <input
+                        type="button"
+                        value="구매취소"
+                        className={styles.cancelBtn}
+                        onClick={e => {
+                          cancelHandler(e, list.detailId);
+                        }}
+                      />
+                    )}
+                    {list.done ? null : (
+                      <input
+                        type="button"
+                        value="구매확정"
+                        className={styles.confirmBtn}
+                        onClick={e => confirmHandler(e, list.detailId)}
+                        disabled={list.done ? true : false}
+                      />
+                    )}
+                    <input
+                      type="button"
+                      value="상세정보"
+                      className={styles.confirmBtn}
+                      onClick={e => {
+                        showDetailsHandler(e, list.detailId);
+                      }}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default MyOrder;
